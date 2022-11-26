8f77ecc43a2a4823e181b273fe945dabaf3ee349
adarsht00001 <adarsht00001@gmail.com>
2022-11-26 13:43:10 +0530

    code formating

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,21 @@
-const MongoClient=require('mongodb').MongoClient
+const MongoClient = require("mongodb").MongoClient;
 
-const state={
-    db:null
-}
+const state = {
+  db: null,
+};
 
-require('dotenv').config()
+require("dotenv").config();
 
-module.exports.connect=function(done){
-    const url='mongodb://localhost:27017/'
-    const dbname='LeosEcommerce'
+module.exports.connect = function (done) {
+  const url = process.env.DATABASE;
+  const dbname = "LeosEcommerce";
 
-    MongoClient.connect(url,(err,data)=>{
-        if(err) return done(err)
-        state.db=data.db(dbname)
-        done()
-    })
-}
-module.exports.get=function(){
-    return state.db
-}
+  MongoClient.connect(url, (err, data) => {
+    if (err) return done(err);
+    state.db = data.db(dbname);
+    done();
+  });
+};
+module.exports.get = function () {
+  return state.db;
+};
